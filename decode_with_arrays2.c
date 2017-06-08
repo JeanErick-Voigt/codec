0dbd8fa7a14b6fa711df8554d358c83c759102a2
Jean-Erick Voigt <voigtjeanerick@gmail.com>
2017-06-08 10:09:26 -0400

    First case for message payload done. sequence data printing in proper form

--- decode_with_arrays2.c
+++ decode_with_arrays2.c
@@ -65,32 +65,34 @@ typedef struct zergPacketHeader {
 
 
 
-void main()
+void main(int argc, char *argv[])
 {
 	TOPHEADER fileHeader;
-	FILE *fp = fopen("/home/jvoigt/share/capstone_1/pcaps/hello.pcap", "r");
-	if (!fp)
-		printf("File, doesn't exist");
+	FILE *fp = fopen(argv[1], "r");
+	if (fp == NULL){
+		printf("file does not exist");
+		exit(1);
+	}
 	fread(&fileHeader, sizeof(fileHeader), 1, fp);
-	printf("%x\n", fileHeader.magicNumber);
-	printf("%x\n", fileHeader.majorVersion);
-	printf("Link Layer Type -----> %x\n", fileHeader.linklayerType);
-	printf("This is fp of file header %ld\n", ftell(fp));	
+//	printf("%x\n", fileHeader.magicNumber);
+//	printf("%x\n", fileHeader.majorVersion);
+//	printf("Link Layer Type -----> %x\n", fileHeader.linklayerType);
+//	printf("This is fp of file header %ld\n", ftell(fp));	
 
 	PACKETHEADER packet;
-	printf("This is size of packet header %ld", sizeof(packet));
+//	printf("This is size of packet header %ld", sizeof(packet));
 	fread(&packet, sizeof(packet), 1, fp);
-	printf("Length Data captured ---- > %x\n", packet.lengthDataCaptured);
+//	printf("Length Data captured ---- > %x\n", packet.lengthDataCaptured);
 	
 	ETHERNET ether;
-	printf("File pointer of ethernet %ld\n", ftell(fp));
-	printf("size of ethernet %ld", sizeof(ether));
+//	printf("File pointer of ethernet %ld\n", ftell(fp));
+//	printf("size of ethernet %ld", sizeof(ether));
 	fread(&ether, sizeof(ether), 1, fp);
-	printf("Ethernet Type -----> %x\n", ether.ethernetType);
+//	printf("Ethernet Type -----> %x\n", ether.ethernetType);
 	int len = ftell(fp);
-	printf("place in code ---> %d", len);
+//	printf("place in code ---> %d", len);
 
-	printf("location of ip header pointer %ld", ftell(fp));
+//	printf("location of ip header pointer %ld", ftell(fp));
 	IPHEADER ip;
 	int version;
 	int ihl;
@@ -98,55 +100,63 @@ void main()
 	fread(&ip, sizeof(ip), 1, fp);
 	ihl = ip.versionAndIHL & 0x0F;
 	version =  ip.versionAndIHL >> 4;
-	printf("This is ihl\n");
-	printf("THis is  IHL ---> %d", ihl);
-	printf("This is version ---> %d", version);
+//	printf("This is ihl\n");
+//	printf("THis is  IHL ---> %d", ihl);
+//	printf("This is version ---> %d", version);
 	putchar('\n');
-	printf("Ip version and IHL ----> %x\n", ip.versionAndIHL);
-	printf("Ip total length ----> %x\n", ip.iptotalLength);
-	printf("THis is filepointer at ipheader %ld\n", ftell(fp));
-	printf("offsets of version and IHL %ld\n", (long) offsetof(IPHEADER, versionAndIHL));
-	printf("Offsets of dscp and ECN %ld\n", (long) offsetof(IPHEADER, dscpAndECN));
-	printf("offsets of ip total length %ld\n", (long) offsetof(IPHEADER, iptotalLength));
-	printf("offsets of identification %ld\n", (long) offsetof(IPHEADER, Identification));
-	printf("offsets of flags to fragment %ld\n", (long) offsetof(IPHEADER, flagstoFragmentoffset));
-	printf("offsets of source ip address %ld\n", (long) offsetof(IPHEADER, sourceIP));
+//	printf("Ip version and IHL ----> %x\n", ip.versionAndIHL);
+//	printf("Ip total length ----> %x\n", ip.iptotalLength);
+//	printf("THis is filepointer at ipheader %ld\n", ftell(fp));
+//	printf("offsets of version and IHL %ld\n", (long) offsetof(IPHEADER, versionAndIHL));
+//	printf("Offsets of dscp and ECN %ld\n", (long) offsetof(IPHEADER, dscpAndECN));
+//	printf("offsets of ip total length %ld\n", (long) offsetof(IPHEADER, iptotalLength));
+//	printf("offsets of identification %ld\n", (long) offsetof(IPHEADER, Identification));
+//	printf("offsets of flags to fragment %ld\n", (long) offsetof(IPHEADER, flagstoFragmentoffset));
+//	printf("offsets of source ip address %ld\n", (long) offsetof(IPHEADER, sourceIP));
 
 	UDP udp;
-	printf("This is size of udp %ld\n", sizeof(udp));
-	printf("FP location %ld\n", ftell(fp));
+//	printf("This is size of udp %ld\n", sizeof(udp));
+//	printf("FP location %ld\n", ftell(fp));
 	fread(&udp, sizeof(udp), 1, fp);
-	printf("FP location %ld\n", ftell(fp));
-	printf("UDP dest port ----> %x\n", udp.destPort);
-	printf("udp length -----> %x\n", udp.Length);
+//	printf("FP location %ld\n", ftell(fp));
+//	printf("UDP dest port ----> %x\n", udp.destPort);
+//	printf("udp length -----> %x\n", udp.Length);
 
 
 	ZERG zerg;
-	printf("fp at end of UDP is at %ld\n", ftell(fp));
-	printf("Size of Zerg %ld\n", sizeof(zerg));
+//	printf("fp at end of UDP is at %ld\n", ftell(fp));
+//	printf("Size of Zerg %ld\n", sizeof(zerg));
 	fread(&zerg, sizeof(zerg), 1, fp);
-	printf("fp after you read zerg is at %ld\n", ftell(fp));
-	printf("Version and type of ZERG \n");
-	printf("%x\n", zerg.versionToType);
-	printf("Total length: ");
-	printf("THis is source--->%x\n", zerg.sourceZergID);
-	printf("This is destination ----> %x\n", zerg.destinationZergID);
-	printf("This is file pointer before message----> %ld\n", ftell(fp));
+//	printf("fp after you read zerg is at %ld\n", ftell(fp));
+//	printf("Version and type of ZERG \n");
+//	printf("%x\n", zerg.versionToType);
+//	printf("Total length: ");
+//	printf("THis is source--->%x\n", zerg.sourceZergID);
+//	printf("This is destination ----> %x\n", zerg.destinationZergID);
+//	printf("This is file pointer before message----> %ld\n", ftell(fp));
 	
-	printf("This is zerg total 0 %d, zerg at 1 %d, zerg at 2 %d", zerg.totalLength[0], zerg.totalLength[1], zerg.totalLength[2]);
+//	printf("This is zerg total 0 %d, zerg at 1 %d, zerg at 2 %d", zerg.totalLength[0], zerg.totalLength[1], zerg.totalLength[2]);
 	int zergLength  = NTOH3(zerg.totalLength);
 	int sequence = 	NTOH4(zerg.sequenceID);
 	int messageLength = zergLength - 12;
 	
-	int type = zerg.versionToType & 0xF;
-	printf("This is type %d", type);
+	int type = zerg.versionToType & 0xF;  // This is type of message
+	int zergVersion = zerg.versionToType >> 4;  // This is version
+	printf("Version: %d\n", zergVersion);
 
 	//if message type = 0  it is a message and can do this
-	char * messagePayload = (char*) malloc((messageLength + 1) * sizeof(char));
-	fread(messagePayload, messageLength, 1, fp);
-	printf("THis is file pointer after message ---> %ld", ftell(fp));
-	messagePayload[messageLength] = '\0';
-	printf("%s", messagePayload);	 
+	// This is the message payload branch
+	switch(type)
+	{
+		case 0:
+			;
+			char * messagePayload; 
+			messagePayload = (char*) malloc((messageLength + 1) * sizeof(char));
+			fread(messagePayload, messageLength, 1, fp);
+			messagePayload[messageLength] = '\0';
+			printf("Sequence: %d\n", sequence);
+			printf("%s", messagePayload);
+	}	 
 	fclose(fp);
 }
 
