c7225e563211388c494c54ac865cea20e5e5af9f
Jean-Erick Voigt <voigtjeanerick@gmail.com>
2017-06-12 13:56:09 -0400

    Decoder complete with loop and float conversions.  all values accounted for.  error checking must be done after encoder

--- decode.c
+++ decode.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <stdint.h>
 #include <stddef.h>
+#include <math.h>
 
 #define NTOH2(x) (((x << 8) & 65280) + (x >> 8))
 #define NTOH3(x) ((int) x[0] << 16) | ((int) (x[1]) << 8) | ((int) (x[2]))
@@ -13,6 +14,10 @@ long unsigned swap32(long unsigned val);
 char * commandOption(int instruction);
 uint64_t ntoh64(uint8_t number[8]);
 int fileSize(FILE *fp);
+int hexToDec(int x);
+double convert64ToDouble(uint64_t num);
+double convert32ToDouble(uint32_t num);
+
 
 typedef struct PcapFileHeader {
 	uint32_t magicNumber;  // File_Type_ID
@@ -103,17 +108,47 @@ void main(int argc, char *argv[])
 	// CHECK file size before while loop, function automatically rewinds file
 	int fullSize = fileSize(fp); 
 	//int *currentSize = ftell(fp); 	
-	printf("This is file size %d\n", fullSize);
+	//printf("This is file size %d\n", fullSize);
+
+
 //Start while loop here
 //Indent all the information below this line for while loop
-	while(ftell(fp) < fullSize){
+	//int packetLength = hexToInt(packet.legnthDataCaptured);
+	//printf("this is packet Length %d\n", packetLength);
+	//while(ftell(fp) < fullSize){
 		
-		fread(&fileHeader, sizeof(fileHeader), 1, fp);
+	
 
+	fread(&fileHeader, sizeof(fileHeader), 1, fp);
+	int count = 0;
+	int endLength = 0;
+	
+	while(ftell(fp) < fullSize)
+	{
+
+		count++;
+		printf("Packet number %d\n", count);
 		PACKETHEADER packet;
 		fread(&packet, sizeof(packet), 1, fp);
+		int packetLength;
+		//packetLength = hexToDec(packet.lengthDataCaptured);
+		//printf("this is packet Length %d\n", packetLength);
+	//	printf("This is packet length of data %d\n", packet.lengthDataCaptured);
+	//	printf("This is the old fp pointer %d\n", ftell(fp));		
+		
+		packetLength = (int) packet.lengthDataCaptured;
+		fseek(fp, packetLength, SEEK_CUR);
+		endLength = ftell(fp);
+		//printf("This is end of packet %d\n", endLength);
+		//printf("This is file pointer at next packet at next pcap packet header %ld\n", ftell(fp));
+		fseek(fp, -packetLength, SEEK_CUR);
+		//printf("This is the new fp %ld\n", ftell(fp));
+		//int y = 50;
+		//if (y < packet.lengthDataCaptured){
+		//	printf("y --> %d is less than length of data captured %d \n", y, packet.lengthDataCaptured);
+		//}
 
-
+	
 		ETHERNET ether;
 		fread(&ether, sizeof(ether), 1, fp);
 		int len = ftell(fp);
@@ -137,9 +172,10 @@ void main(int argc, char *argv[])
 		int zergSourceID = NTOH2(zerg.sourceZergID);
 		int zergDestinationID = NTOH2(zerg.destinationZergID);
 		int zergLength  = NTOH3(zerg.totalLength);
-		int sequence = 	NTOH4(zerg.sequenceID);
+		int sequence = NTOH4(zerg.sequenceID);
+		//int sequence = ntoh(zerg.sequenceID);
 		int messageLength = zergLength - 12;
-		printf("Message length of zerg message %d\n", messageLength);
+		//printf("Message length of zerg message %d\n", messageLength);
 		int type = zerg.versionToType & 0xF;  // This is type of message
 		int zergVersion = zerg.versionToType >> 4;  // This is version
 		printf("Version: %d\n", zergVersion);
@@ -147,10 +183,10 @@ void main(int argc, char *argv[])
 		STATUSPAYLOAD status;
 		//if message type = 0  it is a message and can do this
 		// This is the message payload branch
-		printf("Sequence: %x\n", sequence);
+		printf("Sequence: %u\n", sequence);
 		printf("From: %d\n", zergSourceID);
 		printf("To: %d\n", zergDestinationID);
-		printf("Ip total length %d\n", ipLength);
+		//printf("Ip total length %d\n", ipLength);
 		//char * payloadLength = (char*) malloc((
 		char * messagePayload;
 		messagePayload = (char*) malloc((messageLength + 1) * sizeof(char));
@@ -210,8 +246,9 @@ void main(int argc, char *argv[])
 					fread(&parameter2, sizeof(parameter2), 1, fp);
 					parameter1 = NTOH2(parameter1);
 					int nParameter2 = NTOH4(parameter2);
+					double nParameter2Float = convert32ToDouble(nParameter2);
 					if(commandNum == 1){  //GOTO COMMAND
-						printf("   %x  %d\n",  nParameter2, parameter1);
+						printf("   %lf  %d\n",  nParameter2Float, parameter1);
 					}
 					else if (commandNum == 3){ //RESERVED
 						; // do nothing
@@ -219,13 +256,13 @@ void main(int argc, char *argv[])
 					else if (commandNum == 5){  
 						if(parameter1 != 0){ //SETGROUP
 							//True statement and should be ADD OR SUBTRACT
-							printf("   %d ADD\n", nParameter2);
+							printf("   %lf ADD\n", nParameter2Float);
 						}else{
-							printf("   %x SUBTRACT\n", nParameter2);
+							printf("   %lf SUBTRACT\n", nParameter2Float);
 						}
 				
 					}else{  //REPEAT COMMAND
-						printf("   %d\n", nParameter2);
+						printf("   %lf\n", nParameter2Float);
 					}
 				}
 				break;
@@ -233,21 +270,44 @@ void main(int argc, char *argv[])
 			case 3:
 				fread(&gpsDataPayload, sizeof(gpsDataPayload), 1, fp);
 				uint64_t lat = ntoh64(gpsDataPayload.latitude);
+				double newLat = convert64ToDouble(lat);
+
 				uint64_t longitude = ntoh64(gpsDataPayload.longitude);
+				double newLong = convert64ToDouble(longitude);
+								
 				uint32_t altitude = NTOH4(gpsDataPayload.altitude);
+				double alt32 = convert32ToDouble(altitude);		
+
 				uint32_t bearing = NTOH4(gpsDataPayload.bearing);
+				double bear = convert32ToDouble(bearing);
+
 				uint32_t gpsSpeed = NTOH4(gpsDataPayload.speed);
+				double gpsNSpeed = convert32ToDouble(gpsSpeed);
+
 				uint32_t accuracy = NTOH4(gpsDataPayload.accuracy);
-				printf("Latitude   :  %lx\n", lat);
-				printf("Longitude  :  %lx\n", longitude);
-				printf("Altitude   :  %x\n", altitude);
-				printf("Bearing    :  %x\n", bearing);
-				printf("Speed      :  %x\n", gpsSpeed);
-				printf("Accuracy    :  %x\n", accuracy);
+				double acc = convert32ToDouble(accuracy);
+
+				printf("Latitude   :  %lf degrees\n", newLat);
+				printf("Longitude  :  %lf degrees\n", newLong);
+				//printf("Reverse altitude in hex %x\n", altitude);
+				printf("Altitude reversed and floated  :  %f fathoms\n", alt32);
+				//printf("Non reversed altitude in hex %lx\n", gpsDataPayload.altitude);
+				//printf("Non reversed float value of altitude %f\n", convert32ToDouble(gpsDataPayload.altitude));
+				//printf("Reverse altitude in hex %lx\n", altitude);
+				printf("Bearing    :  %f degrees\n", bear);
+				//printf("Hex of speed reversed %x\n", gpsSpeed);
+				
+				printf("Speed      :  %fm/s\n", gpsNSpeed);
+				printf("Accuracy   :  %fm\n", acc);
 				break;
 		}
+		printf("\n***********************************\n");
+		int difference = endLength - ftell(fp);
+		//printf("this is difference between end of packet and current position of file pointer %d\n", difference);
+		if(difference != 0){
+			fseek(fp, difference, SEEK_CUR);
+		}		
 		
-		//fclose(fp);
 	}
 	fclose(fp);
 }
@@ -366,4 +426,51 @@ int fileSize(FILE *fp)
 	int size = ftell(fp);
 	rewind(fp);
 	return(size);
+}
+
+int hexToDec(int x)
+{
+	int decimalNum = 0;
+	int remainder = 0;
+	int count = 0;
+	while(x > 0)
+	{
+		remainder = x % 10;
+		decimalNum = (decimalNum + remainder) * pow(16, count);
+		x = x/10;
+		count++;
+	}
+	return(decimalNum);
+}
+
+//credit to Daniel Roberts
+double convert64ToDouble(uint64_t num)
+{
+	uint8_t sign;
+	uint16_t exponent;
+	uint64_t mantissa;
+	double result = 0;
+
+	sign = num >> 63;
+	exponent = (num >> 52 & 0x7FF) - 1023;
+	mantissa = num & 0xFFFFFFFFFFFFF;
+	result = (mantissa *pow(2, -52)) + 1;
+	result *= pow(1, sign) * pow(2, exponent);
+	return(result);
+
+}
+
+double convert32ToDouble(uint32_t num)
+{
+	uint8_t sign, exponent;
+	uint32_t mantissa;
+	double result = 0;
+	if(num > 0){	
+		sign = num >> 31;
+		exponent = (num >> 23 & 0xFF) - 127;
+		mantissa = num & 0x7FFFFF;
+		result = (mantissa *pow(2, -23)) + 1;
+		result *= pow(1, sign) * pow(2, exponent);
+	}	
+	return(result);
 }
